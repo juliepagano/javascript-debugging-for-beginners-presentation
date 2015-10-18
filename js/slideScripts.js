596145fabfb2446289c0f57f7797479c22e6df69
Julie Pagano <julie.pagano@gmail.com>
2015-10-18 16:19:03 -0700

    Continue adding content.

---
+++ js/slideScripts.js
@@ -0,0 +1,9 @@
+// This intentionally has an error. Can you find it?
+function alertSpice() {
+  var message = 'THE SPICE MUST FLOW';
+  alert(messages);
+}
+
+var exampleVariable = ["walk", "without", "rhythm"];
+
+var foo = 'gom jabbar';
